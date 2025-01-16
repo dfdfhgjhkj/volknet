1b0d664f3bc1e4c0bab1dbee37ff3dd4a8f8b76f
scfwe <58800155+dfdfhgjhkj@users.noreply.github.com>
2025-01-17 02:14:22 +0800

    Update ButtonRPC.hpp

--- include/ButtonRPC.hpp
+++ include/ButtonRPC.hpp
@@ -1,41 +1,87 @@
-/**
-*
-*	buttonrpc library
-*	Copyright 2018-04-28 Button
-*
-*/
 
-#pragma once
-#include <string>
+
+
+#ifndef _BUTTONRPC_H_
+#define _BUTTONRPC_H_
+
+#include <iostream>
 #include <map>
-#include <string>
-#include <sstream>
 #include <functional>
+#include <tuple>
 #include <memory>
-#include <zmq.hpp>
+#include "Serializer.hpp"
+#if defined(_WIN32)
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib,"Mswsock.lib")
+#endif
+
+
+
+#define NN_STATIC_LIB 1
+#include <nanomsg/nn.h>
+#include <nanomsg/bus.h> 
+#include <nanomsg/pair.h>
 #include "Serializer.hpp"
 
 
-class Serializer;
+class nn_socket_pp
+{
+	int socket;
+public:
+	nn_socket_pp()
+	{
+		this->socket = nn_socket(1, NN_PAIR);
+	}
 
+	void close()
+	{
+		nn_close(this->socket);
+	}
+	int bind(const char* url)
+	{
+		return nn_bind(socket, url);
+
+	}
+	int send(const char* data, int size)
+	{
+		return nn_send(socket, data, size, 0);
+	}
+
+	int recv(void** data)
+	{
+		return nn_recv(socket, data, NN_MSG, 0);
+	}
+	int connect(const char* url)
+	{
+		while (nn_connect(socket, url)<0)
+		{
+
+		}
+		return 0;
+	}
+	int set_timeout(int timeout)
+	{
+		return nn_setsockopt(socket, NN_SOL_SOCKET, NN_LINGER, &timeout, sizeof(timeout));
+	}
+
+};
 template<typename T>
-struct type_xx{	typedef T type; };
+struct type_xx { typedef T type; };
 
 template<>
-struct type_xx<void>{ typedef int8_t type; };
-
+struct type_xx<void> { typedef int8_t type; };
 
 // 打包帮助模板
-template<typename Tuple, std::size_t... Is>
-void package_params_impl(Serializer& ds, const Tuple& t, std::index_sequence<Is...>)
+template<typename Tuple, std::size_t... Index>
+void package_Args_impl(Serializer& sr, const Tuple& t, std::index_sequence<Index...>)
 {
-	initializer_list<int>{((ds << std::get<Is>(t)), 0)...};
+	std::initializer_list<int> { (sr << std::get<Index>(t), 0)... };
 }
 
 template<typename... Args>
-void package_params(Serializer& ds, const std::tuple<Args...>& t)
+void package_Args(Serializer& sr, const std::tuple<Args...>& t)
 {
-	package_params_impl(ds, t, std::index_sequence_for<Args...>{});
+	package_Args_impl(sr, t, std::index_sequence_for<Args...>{});
 }
 
 // 用tuple做参数调用函数模板类
@@ -52,306 +98,242 @@ decltype(auto) invoke(Function&& func, Tuple&& t)
 	return invoke_impl(std::forward<Function>(func), std::forward<Tuple>(t), std::make_index_sequence<size>{});
 }
 
-// 调用帮助类，主要用于返回是否void的情况
-template<typename R, typename F, typename ArgsTuple>
-typename std::enable_if<std::is_same<R, void>::value, typename type_xx<R>::type >::type
-call_helper(F f, ArgsTuple args) {
-	invoke(f, args);
-	return 0;
-}
-
 template<typename R, typename F, typename ArgsTuple>
 typename std::enable_if<!std::is_same<R, void>::value, typename type_xx<R>::type >::type
-call_helper(F f, ArgsTuple args) {
+call_helper(F f, ArgsTuple args)
+{
 	return invoke(f, args);
 }
 
-// rpc 类定义
-class buttonrpc
-{
-public:
-	enum rpc_role{
-		RPC_CLIENT,
-		RPC_SERVER
-	};
-	enum rpc_err_code {
-		RPC_ERR_SUCCESS = 0,
-		RPC_ERR_FUNCTIION_NOT_BIND,
-		RPC_ERR_RECV_TIMEOUT
-	};
-
-	// wrap return value
-	template<typename T>
-	class value_t {
-	public:
-		typedef typename type_xx<T>::type type;
-		typedef std::string msg_type;
-		typedef uint16_t code_type;
-
-		value_t() { code_ = 0; msg_.clear(); }
-		bool valid() { return (code_ == 0 ? true : false); }
-		int error_code() { return code_; }
-		std::string error_msg() { return msg_; }
-		type val() { return val_; }
-
-		void set_val(const type& val) { val_ = val; }
-		void set_code(code_type code) { code_ = code; }
-		void set_msg(msg_type msg) { msg_ = msg; }
-
-		friend Serializer& operator >> (Serializer& in, value_t<T>& d) {
-			in >> d.code_ >> d.msg_;
-			if (d.code_ == 0) {
-				in >> d.val_;
-			}
-			return in;
-		}
-		friend Serializer& operator << (Serializer& out, value_t<T> d) {
-			out << d.code_ << d.msg_ << d.val_;
-			return out;
-		}
-	private:
-		code_type code_;
-		msg_type msg_;
-		type val_;
-	};
-
-	buttonrpc();
-	~buttonrpc();
-
-	// network
-	void as_client(std::string ip, int port);
-	void as_server(int port);
-	void send(zmq::message_t& data);
-	void recv(zmq::message_t& data);
-	void set_timeout(uint32_t ms);
-	void run();
-
-public:
-	// server
-	template<typename F>
-	void bind(std::string name, F func);
-
-	template<typename F, typename S>
-	void bind(std::string name, F func, S* s);
-
-	// client
-	template<typename R, typename... Params>
-	value_t<R> call(std::string name, Params... ps) {
-		using args_type = std::tuple<typename std::decay<Params>::type...>;
-		args_type args = std::make_tuple(ps...);
-
-		Serializer ds;
-		ds << name;
-		package_params(ds, args);
-		return net_call<R>(ds);
-	}
-
-	template<typename R>
-	value_t<R> call(std::string name) {
-		Serializer ds;
-		ds << name;
-		return net_call<R>(ds);
-	}
-
-private:
-	Serializer* call_(std::string name, const char* data, int len);
-
-	template<typename R>
-	value_t<R> net_call(Serializer& ds);
-
-	template<typename F>
-	void callproxy(F fun, Serializer* pr, const char* data, int len);
-
-	template<typename F, typename S>
-	void callproxy(F fun, S* s, Serializer* pr, const char* data, int len);
-
-	// 函数指针
-	template<typename R, typename... Params>
-	void callproxy_(R(*func)(Params...), Serializer* pr, const char* data, int len) {
-		callproxy_(std::function<R(Params...)>(func), pr, data, len);
-	}
-
-	// 类成员函数指针
-	template<typename R, typename C, typename S, typename... Params>
-	void callproxy_(R(C::* func)(Params...), S* s, Serializer* pr, const char* data, int len) {
-
-		using args_type = std::tuple<typename std::decay<Params>::type...>;
-
-		Serializer ds(StreamBuffer(data, len));
-		constexpr auto N = std::tuple_size<typename std::decay<args_type>::type>::value;
-		args_type args = ds.get_tuple < args_type >(std::make_index_sequence<N>{});
-
-		auto ff = [=](Params... ps)->R {
-			return (s->*func)(ps...);
-		};
-		typename type_xx<R>::type r = call_helper<R>(ff, args);
-
-		value_t<R> val;
-		val.set_code(RPC_ERR_SUCCESS);
-		val.set_val(r);
-		(*pr) << val;
-	}
-
-	// functional
-	template<typename R, typename... Params>
-	void callproxy_(std::function<R(Params... ps)> func, Serializer* pr, const char* data, int len) {
-		
-		using args_type = std::tuple<typename std::decay<Params>::type...>;
-
-		Serializer ds(StreamBuffer(data, len));
-		constexpr auto N = std::tuple_size<typename std::decay<args_type>::type>::value;
-		args_type args = ds.get_tuple < args_type > (std::make_index_sequence<N>{});
-
-		typename type_xx<R>::type r = call_helper<R>(func, args);
-
-		value_t<R> val;
-		val.set_code(RPC_ERR_SUCCESS);
-		val.set_val(r);
-		(*pr) << val;
-	}
-
-private:
-	std::map<std::string, std::function<void(Serializer*, const char*, int)>> m_handlers;
-
-	zmq::context_t m_context;
-	std::unique_ptr<zmq::socket_t, std::function<void(zmq::socket_t*)>> m_socket;
-
-	rpc_err_code m_error_code;
-
-	int m_role;
+enum rpc_role {
+	RPC_CLIENT,
+	RPC_SERVER
+};
+enum rpc_errcode {
+	RPC_ERR_SUCCESS = 0,
+	RPC_ERR_FUNCTION_NOT_REGUIST,
+	RPC_ERR_RECV_TIMEOUT
 };
 
-inline buttonrpc::buttonrpc() : m_context(1){
-	m_error_code = RPC_ERR_SUCCESS;
-}
-
-inline buttonrpc::~buttonrpc(){
-	m_context.close();
-}
-
-// network
-inline void buttonrpc::as_client( std::string ip, int port )
+template <typename T>
+class response_t
 {
-	m_role = RPC_CLIENT;
-	m_socket = std::unique_ptr<zmq::socket_t, std::function<void(zmq::socket_t*)>>(new zmq::socket_t(m_context, ZMQ_REQ), [](zmq::socket_t* sock){ sock->close(); delete sock; sock =nullptr;});
-	ostringstream os;
-	os << "tcp://" << ip << ":" << port;
-	m_socket->connect (os.str());
-}
-
-inline void buttonrpc::as_server( int port )
-{
-	m_role = RPC_SERVER;
-	m_socket = std::unique_ptr<zmq::socket_t, std::function<void(zmq::socket_t*)>>(new zmq::socket_t(m_context, ZMQ_REP), [](zmq::socket_t* sock){ sock->close(); delete sock; sock =nullptr;});
-	ostringstream os;
-	os << "tcp://*:" << port;
-	m_socket->bind (os.str());
-}
-
-inline void buttonrpc::send( zmq::message_t& data )
-{
-	m_socket->send(data);
-}
-
-inline void buttonrpc::recv( zmq::message_t& data )
-{
-	m_socket->recv(&data);
-}
-
-inline void buttonrpc::set_timeout(uint32_t ms)
-{
-	// only client can set
-	if (m_role == RPC_CLIENT) {
-		m_socket->setsockopt(ZMQ_RCVTIMEO, ms);
+public:
+	response_t()
+		: code_(RPC_ERR_SUCCESS)
+	{
+		message_.clear();
 	}
-}
+	int code() const { return code_; }
+	std::string message() const { return message_; }
+	T value() const { return value_; }
 
-inline void buttonrpc::run()
-{
-	// only server can call
-	if (m_role != RPC_SERVER) {
-		return;
+	void set_value(const T& val) { value_ = val; }
+	void set_code(int code) { code_ = (int)code; }
+	void set_message(const std::string& msg) { message_ = msg; }
+
+	friend Serializer& operator>>(Serializer& in, response_t<T>& d)
+	{
+		in >> d.code_ >> d.message_;
+		if (d.code_ == 0)
+			in >> d.value_;
+		return in;
 	}
-	while (1){
-		zmq::message_t data;
-		recv(data);
-		StreamBuffer iodev((char*)data.data(), data.size());
-		Serializer ds(iodev);
-
-		std::string funname;
-		ds >> funname;
-		Serializer* r = call_(funname, ds.current(), ds.size()- funname.size());
-
-		zmq::message_t retmsg (r->size());
-		memcpy (retmsg.data (), r->data(), r->size());
-		send(retmsg);
-		delete r;
+	friend Serializer& operator<<(Serializer& out, response_t<T>& d)
+	{
+		out << d.code_ << d.message_ << d.value_;
+		return out;
 	}
-}
 
-// 处理函数相关
+private:
+	int code_;
+	std::string message_;
+	T value_;
+};
 
-inline Serializer* buttonrpc::call_(std::string name, const char* data, int len)
+class ButtonRPC
 {
-	Serializer* ds = new Serializer();
-	if (m_handlers.find(name) == m_handlers.end()) {
-		(*ds) << value_t<int>::code_type(RPC_ERR_FUNCTIION_NOT_BIND);
-		(*ds) << value_t<int>::msg_type("function not bind: " + name);
-		return ds;
+public:
+	ButtonRPC() : context_(1) {}
+	~ButtonRPC() { context_=NULL; }
+
+	void as_client(std::string_view ip, int port)
+	{
+		role_ = RPC_CLIENT;
+		socket_ = std::unique_ptr<nn_socket_pp, std::function<void(nn_socket_pp*)>>(new nn_socket_pp(), [](nn_socket_pp* sock)
+			{
+				sock->close();
+				delete sock;
+				sock = nullptr;
+			});
+		std::string addr = "tcp://" + std::string(ip) + ":" + std::to_string(port);
+		socket_->connect(addr.c_str());
 	}
-	auto fun = m_handlers[name];
-	fun(ds, data, len);
-	ds->reset();
-	return ds;
-}
 
-template<typename F>
-inline void buttonrpc::bind( std::string name, F func )
-{
-	m_handlers[name] = std::bind(&buttonrpc::callproxy<F>, this, func, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
-}
-
-template<typename F, typename S>
-inline void buttonrpc::bind(std::string name, F func, S* s)
-{
-	m_handlers[name] = std::bind(&buttonrpc::callproxy<F, S>, this, func, s, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
-}
-
-template<typename F>
-inline void buttonrpc::callproxy( F fun, Serializer* pr, const char* data, int len )
-{
-	callproxy_(fun, pr, data, len);
-}
-
-template<typename F, typename S>
-inline void buttonrpc::callproxy(F fun, S * s, Serializer * pr, const char * data, int len)
-{
-	callproxy_(fun, s, pr, data, len);
-}
-
-template<typename R>
-inline buttonrpc::value_t<R> buttonrpc::net_call(Serializer& ds)
-{
-	zmq::message_t request(ds.size() + 1);
-	memcpy(request.data(), ds.data(), ds.size());
-	if (m_error_code != RPC_ERR_RECV_TIMEOUT) {
-		send(request);
+	void as_server(std::string_view ip, int port)
+	{
+		role_ = RPC_SERVER;
+		socket_ = std::unique_ptr<nn_socket_pp, std::function<void(nn_socket_pp*)>>(new nn_socket_pp(), [](nn_socket_pp* sock)
+			{
+				sock->close();
+				delete sock;
+				sock = nullptr;
+			});
+		std::string addr = "tcp://" + std::string(ip) + ":" + std::to_string(port);
+		socket_->bind(addr.c_str());
 	}
-	zmq::message_t reply;
-	recv(reply);
-	value_t<R> val;
-	if (reply.size() == 0) {
-		// timeout
-		m_error_code = RPC_ERR_RECV_TIMEOUT;
-		val.set_code(RPC_ERR_RECV_TIMEOUT);
-		val.set_msg("recv timeout");
+
+	bool send(const char* data,int size)
+	{
+
+		int res = socket_->send(data,size);
+		return res;
+	}
+	int recv(void** data)
+	{
+		return socket_->recv(data);
+	}
+	void set_timeout(uint32_t ms)
+	{
+		if (role_ == RPC_CLIENT)
+		{
+			socket_->set_timeout(ms);
+		}
+
+	}
+
+	void run()
+	{
+		while (1)
+		{
+			void *data=nullptr;
+			int data_size=this->recv(&data);
+			std::string a((char*)data, data_size);
+			nn_freemsg(data);
+
+			Buffer::ptr buffer = std::make_shared<Buffer>((char*)data, data_size);
+			Serializer sr(buffer);
+			std::string funcname;
+
+			sr >> funcname;
+			Serializer result;			
+			if (mapFunctions_.find(funcname) == mapFunctions_.end())
+			{
+
+				std::cout << "function not regist, name=" << funcname << std::endl;
+				result << static_cast<int>(RPC_ERR_FUNCTION_NOT_REGUIST);
+				result << std::string("function not bind: " + funcname);
+			}
+			auto func = mapFunctions_[funcname];
+			func(&result, (char*)sr.data(), sr.size());
+			//Serializer* rptr = call_(funcname, sr.data(), sr.size());
+			this->send(result.data(), result.size());
+		}
+	}
+	template <typename R, typename... Args>
+	response_t<R> call(const std::string& name, Args... args)
+	{
+		using args_type = std::tuple<typename std::decay<Args>::type...>;
+		args_type as = std::make_tuple(args...);
+		Serializer sr;
+		sr << name;
+		package_Args(sr, as);
+		return net_call<R>(sr);
+	}
+
+	template <typename F>
+	void regist(const std::string& name, F func)
+	{
+		mapFunctions_[name] = std::bind(&ButtonRPC::callproxy<F>, this, func, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+	}
+
+	template<typename F, typename S>
+	void regist(const std::string& name, F func, S* s)
+	{
+		mapFunctions_[name] = std::bind(&ButtonRPC::callproxy<F, S>, this, func, s, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+	}
+
+	template<typename F>
+	void callproxy(F func, Serializer* pr, const char* data, int len)
+	{
+		callproxy_(func, pr, data, len);
+	}
+	template<typename F, typename S>
+	void callproxy(F func, S* s, Serializer* pr, const char* data, int len)
+	{
+		callproxy_(func, s, pr, data, len);
+	}
+
+	// 函数指针
+	template<typename R, typename... Args>
+	void callproxy_(R(*func)(Args...), Serializer* pr, const char* data, int len) {
+		callproxy_(std::function<R(Args...)>(func), pr, data, len);
+	}
+
+	template<typename R, typename C, typename S, typename... Args>
+	void callproxy_(R(C::* func)(Args...), S* s, Serializer* pr, const char* data, int len) {
+
+		using args_type = std::tuple<typename std::decay<Args>::type...>;
+
+		Serializer sr(std::make_shared<Buffer>(data, len));
+		args_type as = sr.get_tuple<args_type>(std::index_sequence_for<Args...>{});
+
+		auto ff = [=](Args... args)->R {
+			return (s->*func)(args...);
+			};
+		typename type_xx<R>::type r = call_helper<R>(ff, as);
+
+		response_t<R> response;
+		response.set_code(RPC_ERR_SUCCESS);
+		response.set_message("success");
+		response.set_value(r);
+		(*pr) << response;
+	}
+
+	template<typename R, typename... Args>
+	void callproxy_(std::function<R(Args... args)> func, Serializer* pr, const char* data, int len)
+	{
+		using args_type = std::tuple<typename std::decay<Args>::type...>;
+
+		Serializer sr(std::make_shared<Buffer>(data, len));
+		args_type as = sr.get_tuple<args_type>(std::index_sequence_for<Args...>{});
+
+		typename type_xx<R>::type r = call_helper<R>(func, as);
+
+		response_t<R> response;
+		response.set_code(RPC_ERR_SUCCESS);
+		response.set_message("success");
+		response.set_value(r);
+		(*pr) << response;
+	}
+
+	template<typename R>
+	response_t<R> net_call(Serializer& sr)
+	{
+		std::vector<char> request;
+		request.resize(sr.size()+1);
+		memcpy(request.data(), sr.data(), sr.size());
+		this->send(request.data(), sr.size());
+		void* reply=nullptr;
+		int reply_size= this->recv(&reply);
+		response_t<R> val;
+		if (reply_size<=0)
+		{
+			val.set_code(RPC_ERR_RECV_TIMEOUT);
+			val.set_message("recv timeout");
+			return val;
+		}
+		Serializer res((const char*)reply, reply_size);
+		nn_freemsg(reply);
+		res >> val;
 		return val;
 	}
-	m_error_code = RPC_ERR_SUCCESS;
-	ds.clear();
-	ds.write_raw_data((char*)reply.data(), reply.size());
-	ds.reset();
 
-	ds >> val;
-	return val;
-}
+private:
+	std::map<std::string, std::function<void(Serializer*, const char*, int)>> mapFunctions_;
+	int context_;
+	std::unique_ptr<nn_socket_pp, std::function<void(nn_socket_pp*)>> socket_;
+	int role_;
+};
+
+#endif
