b62a0182703eb192a33dab4e0d0a9117d0807a5e
scfwe <58800155+dfdfhgjhkj@users.noreply.github.com>
2025-02-10 04:11:11 +0800

    Update main.cpp

--- Volknet/main.cpp
+++ Volknet/main.cpp
@@ -121,23 +121,23 @@ void subscribe_net(Serializer* out,const char* sePtr, int size)
 int main()
 {
     std::shared_ptr<spdlog::details::thread_pool> tp = std::make_shared<spdlog::details::thread_pool>(1,1);
-    //文件接收机
+    
     std::shared_ptr < spdlog::sinks::rotating_file_sink<std::mutex>> file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/logfile.log", 500 * 1024 * 1024, 1000);
-    // 创建控制台（终端）接收器
+   
     std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    // 创建一个多重接收器的 logger
+   
     std::vector<spdlog::sink_ptr> sinks{ console_sink, file_sink };
     std::shared_ptr<spdlog::async_logger> loggerPtr = std::make_shared<spdlog::async_logger>("multi_sink", sinks.begin(), sinks.end(),tp);
 
-    // 设置全局 logger
+    
     spdlog::set_default_logger(loggerPtr);
-    // 设置日志消息格式
+    
     spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [thread %t] %v");
 
-    // 设置全局日志等级为 debug，所有日志都会输出
+    
     spdlog::set_level(spdlog::level::debug);
 
-    // 开启日志刷新
+    
     spdlog::flush_on(spdlog::level::info);
 
     spdlog::info("VOLKNET online.");
@@ -152,44 +152,43 @@ int main()
     m_namedMtx = std::make_shared<boost::interprocess::named_recursive_mutex>(boost::interprocess::open_or_create, "map");
     std::shared_ptr<TaskScheduler> taskScheduler = std::make_shared<TaskScheduler>(loggerPtr);
 
-    //设置定时函数，第一个值为毫秒数
+    
     std::shared_ptr<std::function<int(UINT64 num,std::string_view name,  std::function<void()>)>> 
         setTimerFuncPtr=
         std::make_shared<std::function<int(UINT64 num,std::string_view name,  std::function<void()>)>>
         (std::bind(&TaskScheduler::SetTimerFunc,taskScheduler,std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
-    //获得定时函数map
-        //设置定时函数，第一个值为毫秒数
+
     std::shared_ptr<std::function<int(std::shared_ptr<ThreadSafeMap<UINT64, std::map<std::string, std::shared_ptr<std::function<void()>>>>>&)>>
         getTimerFuncPtr =
         std::make_shared<std::function<int(std::shared_ptr<ThreadSafeMap<UINT64, std::map<std::string, std::shared_ptr<std::function<void()>>>>>&)>>
         (std::bind(&TaskScheduler::GetTimerFunc, taskScheduler, std::placeholders::_1));
 
-    //添加rpc函数
+
     std::shared_ptr<std::function<int(std::string_view funcName, std::function<int(const std::string&, std::string&)>)>>
         setRPCFuncPtr=
         std::make_shared<std::function<int(std::string_view funcName, std::function<int(const std::string&, std::string&)>)>>
         (std::bind(&TaskScheduler::SetRPCFunc, taskScheduler, std::placeholders::_1, std::placeholders::_2));
-    //获得rpc函数
+
     std::shared_ptr<std::function<int(std::string_view funcName, std::function<int(const std::string&, std::string&)>&)>>
         getRPCFuncPtr=
         std::make_shared<std::function<int(std::string_view funcName, std::function<int(const std::string&, std::string&)>&)>>
         (std::bind(&TaskScheduler::GetRPCFunc, taskScheduler, std::placeholders::_1, std::placeholders::_2));
-    //添加dll函数,any为std::shared_ptr<std::Function>
+
     std::shared_ptr<std::function<int(std::string_view funcName, std::any)>>  
         setDllFuncPtr=
         std::make_shared<std::function<int(std::string_view funcName, std::any)>>
         (std::bind(&TaskScheduler::SetDllFunc, taskScheduler, std::placeholders::_1, std::placeholders::_2));
-    //获得dll函数
+
     std::shared_ptr<std::function<int(std::string_view funcName, std::any&)>>  
         getDllFuncPtr=
         std::make_shared<std::function<int(std::string_view funcName, std::any&)>>
         (std::bind(&TaskScheduler::GetDllFunc, taskScheduler, std::placeholders::_1, std::placeholders::_2));
-    //添加消息函数
+
     std::shared_ptr<std::function<int(std::string_view queueName, std::shared_ptr<MessageBase>)>>
         pushMessagePtr =
         std::make_shared<std::function<int(std::string_view queueName, std::shared_ptr<MessageBase>)>>
         (std::bind(&TaskScheduler::AddMessage, taskScheduler, std::placeholders::_1, std::placeholders::_2));
-    //添加消息处理函数
+
     std::shared_ptr<std::function<int(std::string_view, int, std::string_view, std::function<void(std::shared_ptr<MessageBase>)>)>>  
         pushCallbackFuncPtr=
         std::make_shared<std::function<int(std::string_view,int, std::string_view, std::function<void(std::shared_ptr<MessageBase>)>)>>
@@ -204,12 +203,12 @@ int main()
         spdlog::error("Failed to open the Agents XML file.");
         return 1;
     }
-    // 读取XML文件内容到内存
+
     std::shared_ptr<std::string> xml_contents=std::make_shared<std::string>((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     file.close();
-    // 解析XML文档
+
     doc.parse<rapidxml::parse_default>(&(*xml_contents)[0]);
-    // 获取根元素
+
     rapidxml::xml_node<>* root = doc.first_node("Agents");
 
     std::vector<std::shared_ptr<AgentBase>> agentPtrVector;
@@ -292,22 +291,21 @@ int main()
             AgentBase* newAgent = (*GetAgent)();
             std::shared_ptr<AgentBase> agentPtr(newAgent);
             agentPtrVector.push_back(agentPtr);
-            //设置定时函数，第一个值为毫秒数
+
             agentPtr->m_setTimerFunc= *setTimerFuncPtr;
-            //获得定时函数
-            //设置定时函数，第一个值为毫秒数
+		
             agentPtr->m_getTimerFunc = *getTimerFuncPtr;
-            //添加rpc函数,any为std::shared_ptr<std::Function>
+
             agentPtr->m_setRPCFunc= *setRPCFuncPtr;
-            //获得rpc函数
+
             agentPtr->m_getRPCFunc= *getRPCFuncPtr;
-            //添加dll函数,any为std::shared_ptr<std::Function>
+
             agentPtr->m_setDllFunc= *setDllFuncPtr;
-            //获得dll函数
+
             agentPtr->m_getDllFunc= *getDllFuncPtr;
-            ////添加消息
+
             agentPtr->m_pushMessageFunc= *pushMessagePtr;
-            ////添加消息处理函数
+
             agentPtr->m_pushCallbackFunc = *pushCallbackFuncPtr;
             agentPtr->m_loggerPtr = loggerPtr;
             agentPtr->m_agentName = agentName;
@@ -347,7 +345,6 @@ int main()
     {
         it->get();
     }
-
 	std::shared_ptr<TaskScheduler> taskSchedulerPtr = std::make_shared<TaskScheduler>(loggerPtr);
     taskSchedulerPtr->runTask();
     
