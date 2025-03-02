3bf060f6ae151af366b834554e64ee1d938fb5d5
scfwe <58800155+dfdfhgjhkj@users.noreply.github.com>
2025-03-03 05:12:07 +0800

    Update main.cpp

--- Volknet/main.cpp
+++ Volknet/main.cpp
@@ -222,8 +222,8 @@ int main()
 
     std::shared_ptr<ThreadSafeMap<size_t, std::string>> typenameMapPtr=std::make_shared<ThreadSafeMap<size_t, std::string>>();
 
-    std::shared_ptr<ThreadSafeMap<size_t, std::pair<std::function<std::string(Serializer)>, std::function<Serializer(std::string)>>>> typeToStrFuncMapPtr=
-        std::make_shared<ThreadSafeMap<size_t, std::pair<std::function<std::string(Serializer)>, std::function<Serializer(std::string)>>>>();
+    std::shared_ptr<ThreadSafeMap<size_t, std::pair<std::function<void(std::string& jsonStr, Serializer& SeData )>, std::function<void(std::string& jsonStr, Serializer& SeData)>>>> typeToStrFuncMapPtr=
+        std::make_shared<ThreadSafeMap<size_t, std::pair<std::function<void(std::string& jsonStr, Serializer& SeData)>, std::function<void(std::string& jsonStr, Serializer& SeData)>>>>();
     for (rapidxml::xml_node<>* book = root->first_node("RPCServer"); book; book = book->next_sibling("RPCServer"))
     {
         for (rapidxml::xml_node<>* addr_node = book->first_node("addr"); addr_node; addr_node = addr_node->next_sibling("addr"))
@@ -265,9 +265,9 @@ int main()
         }
     }
 
-    std::shared_ptr<std::function<void(size_t a, const char* b, std::function<std::string(Serializer)>&& toStrFunc, std::function<Serializer(std::string)>&& strToFunc)>> registeTypeFuncPtr =
-        std::make_shared<std::function<void(size_t a, const char* b, std::function<std::string(Serializer)> && toStrFunc, std::function<Serializer(std::string)> && strToFunc)>>(
-        [&](size_t a, const char* b, std::function<std::string(Serializer)>&& toStrFunc, std::function<Serializer(std::string)>&& strToFunc)
+    std::shared_ptr<std::function<void(size_t a, const char* b, std::function<void(std::string& jsonStr, Serializer& SeData)>&& toStrFunc, std::function<void(std::string& jsonStr, Serializer& SeData)>&& strToFunc)>> registeTypeFuncPtr =
+        std::make_shared<std::function<void(size_t a, const char* b, std::function<void(std::string & jsonStr, Serializer & SeData)> && toStrFunc, std::function<void(std::string & jsonStr, Serializer & SeData)> && strToFunc)>>(
+        [&](size_t a, const char* b, std::function<void(std::string& jsonStr, Serializer& SeData)>&& toStrFunc, std::function<void(std::string& jsonStr, Serializer& SeData)>&& strToFunc)
         {
             if (typeToStrFuncMapPtr->find(a) != typeToStrFuncMapPtr->end())
             {
@@ -279,13 +279,34 @@ int main()
                 spdlog::error("typename {} hash collisions with typename{}", b, (*typenameMapPtr)[a]);
                 return;
             }
-
+            std::shared_ptr<std::function<std::string(Serializer SeData)>> ToStrFuncPtr =
+                std::make_shared<std::function<std::string(Serializer  SeData)>>
+                (
+                    [&](Serializer SeData)
+                    {
+                        std::string JsonStr;
+                        toStrFunc(JsonStr, SeData);
+                        return JsonStr;
+                    }
+                );
+            std::shared_ptr<std::function<Serializer(std::string JsonStr)>> StrToFuncPtr =
+                std::make_shared<std::function<Serializer(std::string JsonStr)>>
+                (
+                    [&](std::string JsonStr)
+                    {
+                        Serializer SeData;
+                        toStrFunc(JsonStr, SeData);
+                        return SeData;
+                    }
+                );
             typeToStrFuncMapPtr->insert(std::pair(a, std::make_pair(toStrFunc, strToFunc)));
             typenameMapPtr->insert(std::pair(a, b));
-            RPCServerMap["net"]->regist(std::to_string(a) + "ToStr", toStrFunc);
-            RPCServerMap["shm"]->regist(std::to_string(a) + "ToStr", toStrFunc);
-            RPCServerMap["net"]->regist(std::to_string(a) + "ToSe", strToFunc);
-            RPCServerMap["shm"]->regist(std::to_string(a) + "ToSe", strToFunc);
+            RPCServerMap["net"]->regist(std::to_string(a) + "ToStr", *ToStrFuncPtr);
+
+            RPCServerMap["shm"]->regist(std::to_string(a) + "ToStr", *ToStrFuncPtr);
+            RPCServerMap["net"]->regist(std::to_string(a) + "ToSe", *StrToFuncPtr);
+            RPCServerMap["shm"]->regist(std::to_string(a) + "ToSe", *StrToFuncPtr);
+                
             return;
         });
 
@@ -305,6 +326,10 @@ int main()
                         {
                             dataSe = it->second->call<Serializer>("subscribe_net", std::string(name)).value();
                         }
+                        if (dataSe.size() == 0)
+                        {
+                            return;
+                        }
                     }
                     return;
                 }
@@ -314,10 +339,7 @@ int main()
                     m_namedMtxPtr->lock();
                     dataSS = m_mapPtr->at(nameSS);
                     m_namedMtxPtr->unlock();
-                    if (dataSS.size() == 0)
-                    {
-                        return;
-                    }
+
                     dataSe.input(dataSS.data(), dataSS.size());
                 }
                 return;
@@ -348,6 +370,43 @@ int main()
             }
         );
     
+    //subscribe_str
+    std::shared_ptr<std::function<void(std::string& jsonStr, Serializer& dataSe)>> SeToJsonFuncPtr=
+        std::make_shared<std::function<void(std::string& jsonStr, Serializer & dataSe)>>(
+            [&](std::string& jsonStr, Serializer& dataSe)
+            {
+                size_t a;
+                dataSe >> a;
+                if (typeToStrFuncMapPtr->find(a) == typeToStrFuncMapPtr->end())
+                {
+                    spdlog::error("typename not Registed");
+                    return;
+                }
+                (*typeToStrFuncMapPtr)[a].first(jsonStr,dataSe);
+            }
+        
+        );
+    //publish_str
+    std::shared_ptr<std::function<void(std::string& jsonStr, Serializer& dataSe)>> JsonToSeFuncPtr =
+        std::make_shared<std::function<void(std::string & jsonStr, Serializer & dataSe)>>(
+            [&](std::string& jsonStr, Serializer& dataSe)
+            {
+                nlohmann::json json_ = nlohmann::json::parse(jsonStr);
+                size_t a = json_["typeHash"].get<size_t>();
+
+                if (typeToStrFuncMapPtr->find(a) == typeToStrFuncMapPtr->end())
+                {
+                    spdlog::error("typename not Registed");
+                    return;
+                }                
+                json_.erase("typeHash"); 
+                std::string jsonStr_ = json_.dump();
+                (*typeToStrFuncMapPtr)[a].second(jsonStr_, dataSe);
+            }
+
+        );
+  
+
     for (rapidxml::xml_node<>* book = root->first_node("Agent"); book; book = book->next_sibling("Agent")) 
     {
         rapidxml::xml_node<>* name = book->first_node("name");
@@ -411,6 +470,9 @@ int main()
             agentPtr->m_registeType = *registeTypeFuncPtr;
             agentPtr->m_subscribe = *subscribeFuncPtr;
             agentPtr->m_publish = *publishFuncPtr;
+            agentPtr->m_SeToJson = *SeToJsonFuncPtr;
+            agentPtr->m_JsonToSe= *JsonToSeFuncPtr;
+
             agentPtr->m_mapPtr = m_mapPtr;
             agentPtr->initialize();
 
